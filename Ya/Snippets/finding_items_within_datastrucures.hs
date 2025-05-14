f193f1a0ab2201ba073a980731458a79e94af4e2
Murat Kasimov <iokasimov.m@gmail.com>
2025-05-14 12:57:33 +0700

    Add the very first snippet

---
+++ Ya/Snippets/finding_items_within_datastrucures.hs
@@ -0,0 +1,21 @@
+import "ya" Ya
+import "ya-world" Ya.World
+import "ya-ascii" Ya.ASCII
+import "ya-console" Ya.Console
+
+import "base" Data.Eq (Eq (..))
+import "base" GHC.Num (Integer)
+import "base" System.IO (putStr, print)
+
+instance Setoid AR Integer where
+ equality (These x y) = if x == y then That y else This (These x y)
+
+exact sample current = Continue `la` Interrupt `li` sample `hd'q` current
+
+found x = print "[SUCCESS] Item has been found: " `yuk` Await `hv` print x
+
+havent _ = print "[FAILURE] Item has not been found!"
+
+main = is @(List Integer) [1,2,3]
+ `yokl` Forth `ha` Check `ha` exact 3
+ `yi__` found `la` havent
