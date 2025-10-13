1d0d0de6cdfe11e6c55ee11c37d3051b5185236b
Murat Kasimov <iokasimov.m@gmail.com>
2025-10-13 10:43:56 +0700

    Enhance the snippet on finding element

--- Ya/Snippets/finding-an-element-by-iteration.hs
+++ Ya/Snippets/finding-an-element-by-iteration.hs
@@ -3,19 +3,16 @@ import "ya-world" Ya.World
 import "ya-ascii" Ya.ASCII
 import "ya-console" Ya.Console
 
-import "base" Data.Eq (Eq (..))
-import "base" GHC.Num (Integer)
-import "base" System.IO (putStr, print)
+type String = Nonempty List ASCII
 
-instance Setoid AR Integer where
- equality (These x y) = if x == y then That y else This (These x y)
+print x = x `yokl` Forth `ha` Await `ha` output
 
-exact sample current = Continue `la` Interrupt `li_` sample `lu'q` current
+found x = "[SUCCESS] Item has been found: "
+ `yi` print `ha` is @String `yuk` Await `ha` print `hv` x `yu` Unit
 
-found x = print "[SUCCESS] Item has been found: " `yuk` Await `hv` print x
+havent _ = "[FAILURE] Item has not been found!"
+ `yi` print `ha` is @String `yu` Unit
 
-havent _ = print "[FAILURE] Item has not been found!"
-
-main = is @(List Integer) [1,2,3]
- `yokl` Forth `ha` Check `ha` exact 3
- `yi__` found `la` havent
+main = [A,B,C,D,E,F,G]
+ `yokl` Forth @List `ha` Check `ha` Stops `ha` not `ha` (`lu'q` F)
+ `yi__` found `ha` Alone `ha` Glyph `ha` Letter `ha` Upper `ha` (`hv` Unit) `la` havent
